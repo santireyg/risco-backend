71a512e519d3fed66cd5391d13a48f6f6c0313fd
Santiago Rey Goitia <santiagorey8@gmail.com>
2025-10-02 19:07:48 -0300

    Agregó nuevo CORS

--- app/main.py
+++ app/main.py
@@ -37,6 +37,7 @@ app = FastAPI(
 origins = [
     "http://localhost:3000",
     "https://integrity-frontend-lime.vercel.app",
+    "https://risco-frontend.vercel.app"
     "https://integrity-staging.genovasolutions.com",
     "https://integrity.genovasolutions.com",
 ]
